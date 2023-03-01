dabbdd5dcb3e0947339b5d7800d4c67fd0c5f067
Johannes Alexander Putra <71767003+Itsjohanes@users.noreply.github.com>
2023-03-01 08:37:08 +0700

    initial commit

---
+++ src/swing/Swing.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swing;
+
+/**
+ *
+ * @author acer
+ */
+
+public class Swing {
+
+    
+    public int a = 10;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Swing sw = new Swing();
+        System.out.println(sw.a);
+    }
+    
+}
